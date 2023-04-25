d865d8a80a586d74d76f2d9157be8ba40ae7b4bd
Ethan <info@ethanthl.com>
2023-04-25 17:57:37 +1000

    basic file and server setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./app.css";
+import { Container, Grid } from "@mui/material";
+import tweet from "./images/tweet.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <Grid container sx={{mt: "2rem"}}>
+        <Grid xs={3} container direction="column">
+          <img src={tweet} alt="tweet" height="32" width="32"></img>
+          <h2>#Explore</h2>
+        </Grid>
+        <Grid xs={6}></Grid>
+        <Grid xs={3}>
+          <img src={tweet} alt="tweet" height="32"></img>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
